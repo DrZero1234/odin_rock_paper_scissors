1ce96ff2f0247b68bfbbf3fb5b46fad4bd0fcd1d
M.Kristóf <drzero123456@gmail.com>
2022-03-16 19:48:11 +0100

    Working version with sound

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const buttons = document.querySelector(".selection").querySelectorAll("div");
-
 
 const options = ["Rock", "Paper", "Scissors"]
 
@@ -7,8 +5,8 @@ const options = ["Rock", "Paper", "Scissors"]
 let player_score_int = 0;
 let CPU_score_int = 0;
 
-// Generates the content of the website
-
+// Query selectors
+const buttons = document.querySelector(".selection").querySelectorAll("div");
 const human_score_element =  document.querySelector("#human-score");
 const CPU_score_element = document.querySelector("#cpu-score");
 const last_round_text = document.querySelector("#last-round-text");
@@ -16,14 +14,7 @@ const play_again = document.querySelector("#play-again");
 
 
 
-function reset_game() {
-    player_score_int = 0;
-    CPU_score_int = 0;
-    human_score_element.textContent = player_score_int;
-    CPU_score_element.textContent = CPU_score_int;
-    play_again.style.display = "none";
 
-}
 
 // Generates the CPU´s move
 function computerPlay() {
@@ -31,16 +22,12 @@ function computerPlay() {
     return computerChoice
 }
 
-/* Plays a round of Rock paper scissors. Displays the player and CPU choices
-and returns a value accordingly:
+/* Plays a round of Rock paper scissors. Returns a value based on the result
  1 - Player Won,
- 0 - Draw,
  -1 CPU won
 
  */
 function playRound(playerSelection) {
-    // Displaying the choices for testing purposes
-    console.log(`Player Selection: ${playerSelection}`);
     computerSelection = computerPlay();
     // The player won the round
     if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
@@ -56,46 +43,85 @@ function playRound(playerSelection) {
     }
     
 }
+
+// Plays a specific sound depending on the result (win or lose)
+
+function play_sound(e) {
+    if (e == "win") {
+        audio = new Audio("sounds/win/yes.mp3")
+    } else if (e === "lose") {
+        audio = new Audio("sounds/lost/the-price-is-right-losing-horn.mp3")
+    }
+    audio.play()
+}
+
+/*
+When clicking the play again button this function will reset the game
+- 
+*/
+
+function reset_game() {
+    // Resets the score
+    player_score_int = 0;
+    CPU_score_int = 0;
+    human_score_element.textContent = player_score_int;
+    CPU_score_element.textContent = CPU_score_int;
+    // Hides the Play Again button
+    play_again.style.display = "none";
+    // Makes the rock, paper, scissor buttons clickable again
+    buttons.forEach((button) => {
+        button.style.pointerEvents = "auto";
+    })
+
+}
+
+
 /*
   Simulates 5 round of rock paper scissors and displays the winner of the series.
 
 */
 function game(playerSelection) {
 
-    // playing the game for 5 rounds
+    
     computerSelection = computerPlay()
+    // playing the game for 5 rounds
     if  (player_score_int < 5 && CPU_score_int < 5){
         result = playRound(playerSelection);
-        console.log(result);
-        // asks the user for legitimate choice (Rock, Paper or Scissor)
-        // CPU choice with the help of computerPlay function
-        // Adding to the result based on the turn´s result
+        // If the player won the round 
         if (result === 1) {
             player_score_int++;
             human_score_element.textContent = player_score_int;
-
+        // If the CPU won the round
         } else if (result === -1) {
             CPU_score_int++;
             CPU_score_element.textContent = CPU_score_int;
         }
     }
-    // Evaluating and displaying the winner and the final score
+    // Evaluating and displaying the winner of the game
     if (player_score_int >= 5 || CPU_score_int >= 5) {
         if (player_score_int === 5) {
             last_round_text.textContent = `You have defeated the computer, good job.`;
+            play_sound("win");
             } else if (CPU_score_int === 5){
-            last_round_text.textContent = `The computer won. You gotta practice your random picking skills`;  
+            play_sound("lose");
+            last_round_text.textContent = `The computer won. You gotta practice your random picking skills`;
         }
+        // At the end of the game makes the rock, paper scissors buttons unclickable
+        buttons.forEach((button) => {
+            button.style.pointerEvents = "none";
+        })
+        // The Play again button appears
         play_again.style.display = "block";
         play_again.addEventListener("click", reset_game);
     }
-    return
+    return;
 
     
 }
 
+
+
 buttons.forEach((button) => {
-    console.log(`Id ${button.id}`)
     button.addEventListener("click", () => {
         game(button.id, computerPlay() )
     })
