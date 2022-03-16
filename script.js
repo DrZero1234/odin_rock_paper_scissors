35940acc8d7a0f381e4b973c05b2521dc7c943f9
M.Kristóf <drzero123456@gmail.com>
2022-03-16 18:23:01 +0100

    RPS first working browser version

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const buttons = document.querySelector(".selection").querySelectorAll("div");
 
+
 const options = ["Rock", "Paper", "Scissors"]
+
+
 let player_score_int = 0;
 let CPU_score_int = 0;
 
@@ -9,9 +12,18 @@ let CPU_score_int = 0;
 const human_score_element =  document.querySelector("#human-score");
 const CPU_score_element = document.querySelector("#cpu-score");
 const last_round_text = document.querySelector("#last-round-text");
+const play_again = document.querySelector("#play-again");
 
 
 
+function reset_game() {
+    player_score_int = 0;
+    CPU_score_int = 0;
+    human_score_element.textContent = player_score_int;
+    CPU_score_element.textContent = CPU_score_int;
+    play_again.style.display = "none";
+
+}
 
 // Generates the CPU´s move
 function computerPlay() {
@@ -26,10 +38,10 @@ and returns a value accordingly:
  -1 CPU won
 
  */
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
     // Displaying the choices for testing purposes
     console.log(`Player Selection: ${playerSelection}`);
-    console.log(`Computer Selection: ${computerSelection}`);
+    computerSelection = computerPlay();
     // The player won the round
     if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
         last_round_text.textContent = `You won ${playerSelection} beats ${computerSelection}.`;
@@ -48,10 +60,13 @@ function playRound(playerSelection, computerSelection) {
   Simulates 5 round of rock paper scissors and displays the winner of the series.
 
 */
-function game(playerSelection, computerSelection) {
+function game(playerSelection) {
+
     // playing the game for 5 rounds
+    computerSelection = computerPlay()
     if  (player_score_int < 5 && CPU_score_int < 5){
-        result = playRound(playerSelection, computerSelection);
+        result = playRound(playerSelection);
+        console.log(result);
         // asks the user for legitimate choice (Rock, Paper or Scissor)
         // CPU choice with the help of computerPlay function
         // Adding to the result based on the turn´s result
@@ -65,15 +80,18 @@ function game(playerSelection, computerSelection) {
         }
     }
     // Evaluating and displaying the winner and the final score
+    if (player_score_int >= 5 || CPU_score_int >= 5) {
         if (player_score_int === 5) {
             last_round_text.textContent = `You have defeated the computer, good job.`;
-            return;
-        } else if (CPU_score_int === 5){
-            last_round_text.textContent = `The computer won. You gotta practice your random picking skills`
-            return; 
-
+            } else if (CPU_score_int === 5){
+            last_round_text.textContent = `The computer won. You gotta practice your random picking skills`;  
+        }
+        play_again.style.display = "block";
+        play_again.addEventListener("click", reset_game);
     }
+    return
 
+    
 }
 
 buttons.forEach((button) => {
